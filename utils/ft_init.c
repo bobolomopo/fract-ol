951c8128d0591022289da75cb02bc7a9cfabf196
Julien Andre <jandre@shi-r6-p7.s19.be>
2021-06-01 14:49:07 +0200

    ok

--- utils/ft_init.c
+++ utils/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: jandre <jandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 13:19:50 by jandre            #+#    #+#             */
-/*   Updated: 2021/06/01 13:26:40 by jandre           ###   ########.fr       */
+/*   Updated: 2021/06/01 14:03:47 by jandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void    ft_init_pos_screen(t_display dis)
 {
-    dis.pos.center = 0;
-    dis.pos.dowm_limit = -6;
-    dis.pos.up_limit = 6;
-    dis.pos.right_limit = 6;
-    dis.pos.left_limit = -6;
-    return ;
+	dis.pos.center = 0;
+	dis.pos.dowm_limit = -6;
+	dis.pos.up_limit = 6;
+	dis.pos.right_limit = 6;
+	dis.pos.left_limit = -6;
+	return ;
 }
